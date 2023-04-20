e08ba7fbcf33c5b37de6e2e8b2a30cf3ff243772
Fabio Cavalcante <facaval@microsoft.com>
2023-04-20 16:49:11 -0700

    Termination sample

---
+++ TerminationSample/MyHostedService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Hosting;
+
+internal class MyHostedService : IHostedService
+{
+    private readonly IHostApplicationLifetime _applicationLifetime;
+    private CancellationTokenRegistration _registration;
+
+    public MyHostedService(IHostApplicationLifetime applicationLifetime)
+    {
+        _applicationLifetime = applicationLifetime;
+    }
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _registration =  _applicationLifetime.ApplicationStopping.Register(() => { });
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _registration.Dispose();
+
+        return Task.CompletedTask;
+    }
+}
